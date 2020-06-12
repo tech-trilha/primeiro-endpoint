70d0643fcdd53ab02a48d285f4cd34fa4c31c7e2
Felipe Miranda <felip.simi@gmail.com>
2020-06-12 13:53:13 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/primeiroendpoint/PrimeiroEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.primeiroendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroEndpointApplication.class, args);
+	}
+
+}
